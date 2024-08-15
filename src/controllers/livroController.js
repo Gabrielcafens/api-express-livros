ff01db78f870f50a9461ed181e483502758123fe
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-15 15:30:44 -0300

    feat: refact routes and PUT

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,10 +2,33 @@ import livro from "../models/Livro.js";
 
 class LivroController {
  
-    static async listarLivros (req, res) {
-      const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
+  static async listarLivroPorId (req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado);
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na requisição do livro` });
     }
+  };
+  static async listarLivroPorId (req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado);
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na requisição do livro` });
+    }
+  };
+  static async atualizarLivro (req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "livro atualizado" });
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na atualização` });
+    }
+  };
     static async cadastrarLivro (req, res) {
       try {
         const novoLivro = await livro.create(req.body);
