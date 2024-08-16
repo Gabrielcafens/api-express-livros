fe122e86c864ff0eb9fd88782dc02ff9519c0717
gabrielcafens <gabriel.cafe@ufrpe.br>
2024-08-16 10:47:09 -0300

    feat: busca

--- src/models/index.js
+++ src/models/index.js
@@ -3,17 +3,3 @@ import autores from "./Autor.js";
 import livros from "./Livro.js";
 
 export { autores, livros };
-
-const autorSchema = new mongoose.Schema(
-  {
-    id: {type: String},
-    nome: {
-      type: String,
-      required: [true, "O nome do(a) autor(a) é obrigatório"]
-    },
-    nacionalidade: {type: String}
-  },
-  {
-    versionKey: false
-  }
-);
